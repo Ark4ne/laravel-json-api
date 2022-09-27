faed768151f81fe77e619c5c1cf627f4425c990f
Ark4ne <gui.allegret@gmail.com>
2022-09-27 16:39:06 +0200

    test: add unit test on Fields helper

--- tests/Unit/Support/FieldsTest.php
+++ tests/Unit/Support/FieldsTest.php
@@ -26,4 +26,34 @@ class FieldsTest extends TestCase
     {
         $this->assertEquals($expected, Fields::get(new Request(['fields' => $query]), $type));
     }
+
+    public function testGetWithContext()
+    {
+        Fields::through('test', function () {
+            $this->assertEquals(null, Fields::get(new Request));
+            $this->assertEquals(['a', 'b', 'c'], Fields::get(new Request(['fields' => ['test' => 'a,b,c']])));
+        });
+    }
+
+    public function testHasWithContext()
+    {
+        Fields::through('test', function () {
+            $this->assertEquals(false, Fields::has(new Request, 'b'));
+            $this->assertEquals(true, Fields::has(new Request(['fields' => ['test' => 'a,b,c']]), 'b'));
+        });
+    }
+
+    public function testFailGetWithoutContext()
+    {
+        $this->expectException(\BadMethodCallException::class);
+
+        Fields::get(new Request);
+    }
+
+    public function testFailHasWithoutContext()
+    {
+        $this->expectException(\BadMethodCallException::class);
+
+        Fields::has(new Request, 'test');
+    }
 }
