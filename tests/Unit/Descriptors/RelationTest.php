6d02e20be63029fd9faecd1334a02367539d5b49
Ark4ne <gui.allegret@gmail.com>
2022-09-27 21:08:56 +0200

    test: relation

---
+++ tests/Unit/Descriptors/RelationTest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Test\Unit\Descriptors;
+
+use Ark4ne\JsonApi\Descriptors\Relations\RelationOne;
+use Ark4ne\JsonApi\Resources\Relationship;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\MissingValue;
+use stdClass;
+use Test\app\Http\Resources\UserResource;
+use Test\Support\Reflect;
+use Test\TestCase;
+
+class RelationTest extends TestCase
+{
+    public function testIncluded()
+    {
+        $stub = new RelationOne(UserResource::class, fn() => null);
+
+        $relation = $stub->resolveFor(new Request, new class extends Model {
+        }, 'null');
+
+        $this->assertInstanceOf(Relationship::class, $relation);
+        $this->assertFalse(Reflect::get($relation, 'whenIncluded'));
+
+        $stub->whenIncluded();
+        $relation = $stub->resolveFor(new Request, new class extends Model {
+        }, 'null');
+
+        $this->assertInstanceOf(Relationship::class, $relation);
+        $this->assertTrue(Reflect::get($relation, 'whenIncluded'));
+    }
+
+    public function testMeta()
+    {
+        $stub = new RelationOne(UserResource::class, fn() => null);
+
+        $relation = $stub->resolveFor(new Request, new class extends Model {
+        }, 'null');
+
+        $this->assertInstanceOf(Relationship::class, $relation);
+        $this->assertEmpty(Reflect::get($relation, 'meta'));
+
+        $stub->meta(fn() => ['test']);
+        $relation = $stub->resolveFor(new Request, new class extends Model {
+        }, 'null');
+
+        $this->assertInstanceOf(Relationship::class, $relation);
+        $this->assertInstanceOf(\Closure::class, Reflect::get($relation, 'meta'));
+        $this->assertEquals(['test'], Reflect::get($relation, 'meta')());
+    }
+
+    public function testLinks()
+    {
+        $stub = new RelationOne(UserResource::class, fn() => null);
+
+        $relation = $stub->resolveFor(new Request, new class extends Model {
+        }, 'null');
+
+        $this->assertInstanceOf(Relationship::class, $relation);
+        $this->assertEmpty(Reflect::get($relation, 'links'));
+
+        $stub->links(fn() => ['test']);
+        $relation = $stub->resolveFor(new Request, new class extends Model {
+        }, 'null');
+
+        $this->assertInstanceOf(Relationship::class, $relation);
+        $this->assertInstanceOf(\Closure::class, Reflect::get($relation, 'links'));
+        $this->assertEquals(['test'], Reflect::get($relation, 'links')());
+    }
+
+    public function dataWhenLoaded() {
+        return [
+            ['attr', null, 'attr'],
+            ['attr', fn() => null, 'attr'],
+            ['field', 'field', 'attr'],
+        ];
+    }
+
+    /**
+     * @dataProvider dataWhenLoaded
+     */
+    public function testWhenLoaded($expectedAttr, $relation, $invokedAttr)
+    {
+        $mockModel = $this->getMockForAbstractClass(Model::class, mockedMethods: ['relationLoaded']);
+        $mockModel->expects(self::once())->method('relationLoaded')->with($expectedAttr)->willReturn(false);
+
+        $stub = new RelationOne(UserResource::class, $relation);
+        $stub->whenLoaded();
+
+        $check = Reflect::invoke($stub, 'check', new Request, $mockModel, $invokedAttr);
+        $this->assertFalse($check);
+    }
+
+    public function dataWhenPivotLoaded() {
+        return [
+            ['attr', null, 'attr'],
+            ['attr', fn() => null, 'attr'],
+            ['field', 'field', 'attr'],
+        ];
+    }
+
+    /**
+     * @dataProvider dataWhenPivotLoaded
+     */
+    public function testWhenPivotLoaded($expectedAttr, $relation, $invokedAttr)
+    {
+        $model = new class extends Model {};
+
+        $model->$expectedAttr = new stdClass;
+        $stub = new RelationOne(UserResource::class, $relation);
+        $stub->whenPivotLoaded(stdClass::class);
+
+        $check = Reflect::invoke($stub, 'check', new Request, $model, $invokedAttr);
+        $this->assertTrue($check);
+    }
+}
