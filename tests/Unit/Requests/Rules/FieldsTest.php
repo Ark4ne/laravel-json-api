8c2fdb1e457c7ac061a94afb156b0cce02847442
Yanis <yanis.ghidouche@gmail.com>
2025-10-15 12:12:32 +0400

    fix: update validation rules to use ValidationRule interface and improve error handling

--- tests/Unit/Requests/Rules/FieldsTest.php
+++ tests/Unit/Requests/Rules/FieldsTest.php
@@ -3,35 +3,52 @@
 namespace Test\Unit\Requests\Rules;
 
 use Ark4ne\JsonApi\Requests\Rules\Fields;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Test\app\Http\Resources\UserResource;
 use Test\TestCase;
 
 class FieldsTest extends TestCase
 {
-    public function testPasses()
+    /**
+     * @dataProvider validationDataProvider
+     */
+    #[DataProvider('validationDataProvider')]
+    public function testPasses(mixed $values, array $expected)
     {
         $rule = new Fields(UserResource::class);
+        $errors = [];
 
-        $this->assertFalse($rule->passes(null, 'user,post'));
+        $rule->validate('fields', $values, function($message) use (&$errors) {
+            $errors[] = $message;
+        });
+        $this->assertEquals($expected, $errors);
+    }
 
-        $this->assertTrue($rule->passes(null, [
-            'user' => 'name,email',
-            'post' => 'content',
-        ]));
+    public static function validationDataProvider()
+    {
+        return [
+            // not expected type
+            ['string', ['The selected :attribute is invalid.']],
+            [123, ['The selected :attribute is invalid.']],
+            [null, ['The selected :attribute is invalid.']],
 
-        $this->assertFalse($rule->passes(null, [
-            'user' => 'name,email',
-            'unknown' => 'content',
-        ]));
+            // valid cases
+            [['user' => 'name,email'], []],
+            [['user' => 'name,email', 'post' => 'content'], []],
 
-        $this->assertFalse($rule->passes(null, [
-            'user' => 'name,unknown',
-            'post' => 'content',
-        ]));
-
-        $this->assertFalse($rule->passes(null, [
-            'user' => 'name,email',
-            'post' => 'unknown',
-        ]));
+            // invalid cases
+            [['user' => 'name,email', 'unknown' => 'content'], [
+                'The selected :attribute is invalid.',
+                '"unknown" doesn\'t exists.',
+            ]],
+            [['user' => 'name,unknown', 'post' => 'content'], [
+                'The selected :attribute is invalid.',
+                '"user" doesn\'t have fields "unknown".'
+            ]],
+            [['user' => 'name,email', 'post' => 'unknown'], [
+                'The selected :attribute is invalid.',
+                '"post" doesn\'t have fields "unknown".'
+            ]],
+        ];
     }
 }
